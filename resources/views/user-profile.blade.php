407016934b4500a8b70732c80d57e32cfb06e028
AxelKane0610 <laswellcssd@gmail.com>
2026-05-24 16:35:19 +0700

    test golive

---
+++ resources/views/user-profile.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Project Chevalier</title>
+        <meta charset="utf-8">
+        @vite(['resources/css/app.css', 'resources/js/app.js', 'resources/js/software-ticket-details.js', 'resources/css/icons/themify-icons.css'])
+        
+    </head>
+
+    <body class="background-enable">
+        <x-common-header>   
+            
+        </x-common-header>
+    </body>
+
+</html>
