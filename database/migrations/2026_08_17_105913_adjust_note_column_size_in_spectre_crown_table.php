8d6266f09cf9b719542568b9c0316d9dc7e3c121
OMEN <laswellcssd@gmail.com>
2026-08-17 11:06:12 +0700

    adjust column note

--- database/migrations/2026_08_17_105913_adjust_note_column_size_in_spectre_crown_table.php
+++ database/migrations/2026_08_17_105913_adjust_note_column_size_in_spectre_crown_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('spectre_crown_table', function (Blueprint $table) {
+        Schema::table('spectre_crown_warehouse', function (Blueprint $table) {
             $table->text('note')->nullable()->change();
         });
     }
@@ -23,7 +23,7 @@ return new class extends Migration
     {
         Schema::table('spectre_crown', function (Blueprint $table) {
             //
-            Schema::table('spectre_crown_table', function (Blueprint $table) {
+            Schema::table('spectre_crown_warehouse', function (Blueprint $table) {
                 $table->string('note', 255)->nullable()->change();
             });
         });
